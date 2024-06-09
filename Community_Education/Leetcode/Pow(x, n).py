033840bf8728c9ac949bb6ecb48fac034be38f04
Lemesa Elias <149191865+Lemi-in@users.noreply.github.com>
2024-06-09 15:46:44 +0300

    Create Pow(x, n).py

---
+++ Community_Education/Leetcode/Pow(x, n).py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n < 0:
+            return 1 / self.power(x, -n)
+        else:
+            return self.power(x, n)
+        
+    def power(self, x, n):
+        if n == 0:
+            return 1
+        half = self.power(x, n // 2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
